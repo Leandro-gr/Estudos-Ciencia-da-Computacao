9d351cf92688f31cc9db0db1ae33e2f4fdab8dd3
Leandro de Oliveira <leandro.ult.oliveira@gmail.com>
2023-03-06 18:00:05 -0300

    ADD - Variável Exercícios

---
+++ variables/1_variavel.py
@@ -0,0 +1,7 @@
+#--- Cria uma variavel chamada 'nome' e atribui a ela o valor 'leandro'. printa na tela a variavel 'nome' e recebe o valor 'leandro' como reposta
+
+nome = 'leandro'   
+print(nome)
+
+
+
---
+++ variables/2_soma.py
@@ -0,0 +1,8 @@
+#--- Cria um programa que executa a soma de duas variaveis
+
+a = 10
+b = 20
+
+soma = a + b
+
+print(f'A soma dos números é {soma}')
